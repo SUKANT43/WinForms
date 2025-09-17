93e91494bdba90430696539436a9eb45f3dd09e4
SUKANT43 <sukant98657@gmail.com>
2025-09-17 12:36:20 +0530

    form page

--- Study/wf/wf/Form1.cs
+++ Study/wf/wf/Form1.cs
@@ -16,5 +16,25 @@ namespace wf
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Hello {textBox1.Text} {textBox2.Text}");
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
