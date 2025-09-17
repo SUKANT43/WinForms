e5d764ce910b1dbff8da0ab1e0c7c7e31573bf10
SUKANT43 <sukant98657@gmail.com>
2025-09-17 12:36:53 +0530

    feedback form

---
+++ Excercises/FormApplication/FormApplication/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            richTextBox1.Text = "";
+            MessageBox.Show($"Your feedback submitted successfully\n" +
+                $"Name: {textBox1.Text}\n"+
+                $"Designation: {textBox2.Text}\n"+
+                $"Feedback: {richTextBox1.Text}");
+        }
+    }
+}
