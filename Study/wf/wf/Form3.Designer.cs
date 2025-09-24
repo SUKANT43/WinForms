789f768e92c81aa1ed6f2ea515e58c65055d8146
SUKANT43 <sukant98657@gmail.com>
2025-09-24 10:05:13 +0530

    Scroll view

---
+++ Study/wf/wf/Form3.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace wf
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FormPanel = new System.Windows.Forms.Panel();
+            this.BigButton = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form3";
+
+            this.Controls.Add(this.FormPanel);
+            this.FormPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FormPanel.AutoScroll = true;
+            this.FormPanel.Controls.Add(this.BigButton);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel FormPanel;
+        private System.Windows.Forms.Button BigButton;
+    }
+}
