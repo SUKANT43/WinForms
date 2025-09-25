741fe5ee5c4040e837d98414a877e9331f283328
SUKANT43 <sukant98657@gmail.com>
2025-09-25 16:22:56 +0530

    winforms basics

--- Study/wf/wf/Form1.cs
+++ Study/wf/wf/Form1.cs
@@ -14,38 +14,165 @@ namespace wf
     {
         public Form1()
         {
-            InitializeComponent();
-            label2.Text = "Last Name";
-            label3.Text = "Full Name";
+            //InitializeComponent();
+            //SplitContainer split = new SplitContainer();
+            //split.Dock = DockStyle.Fill;
+            //split.Orientation = Orientation.Vertical; // left & right panels
+            //split.SplitterDistance = 200; // initial width of left panel
+
+            //// Add controls to Panel1 and Panel2
+            //split.Panel1.BackColor = Color.LightBlue;
+            //split.Panel2.BackColor = Color.LightGreen;
+
+            //split.Panel1.Controls.Add(new Button() { Text = "Left Button" });
+            //split.Panel2.Controls.Add(new Button() { Text = "Right Button" });
+
+            //this.Controls.Add(split);
+            //ListView ls = new ListView();
+            //ls.Dock = DockStyle.Fill;
+            //ls.View = View.Details;          // 🔑 must set this to see columns
+            //ls.FullRowSelect = true;         // (optional) selects entire row
+
+            //// Add columns
+            //ls.Columns.Add("Subject", 100);
+            //ls.Columns.Add("Mark", 50);
+
+            //// Add row (first item text is for first column)
+            //ListViewItem l1 = new ListViewItem("Maths");  // goes to "Subject"
+            //l1.SubItems.Add("100");                       // goes to "Mark"
+            //ls.Items.Add(l1);
+
+            //// Add another row
+            //ListViewItem l2 = new ListViewItem("Science");
+            //l2.SubItems.Add("95");
+            //ls.Items.Add(l2);
+
+            //// Add ListView to form
+            //Controls.Add(ls);
+            //MaskedTextBox mtxt = new MaskedTextBox();
+            //mtxt.Location = new Point(20, 20);
+            //mtxt.Mask = "(999) 000-0000";  // Phone number format
+            //mtxt.PromptChar = '_';         // Placeholder character
+            //mtxt.Width = 120;
+
+            //Button btn = new Button();
+            //btn.Text = "Check";
+            //btn.Location = new Point(20, 60);
+
+            //btn.Click += (s, e) =>
+            //{
+            //    if (mtxt.MaskCompleted)
+            //        MessageBox.Show("Phone: " + mtxt.Text);
+            //    else
+            //        MessageBox.Show("Please enter full number!");
+            //};
+
+            //this.Controls.Add(mtxt);
+            //this.Controls.Add(btn);
+            //this.Text = "NotifyIcon Demo";
+            //this.Size = new Size(300, 200);
+
+            //NotifyIcon notifyIcon = new NotifyIcon();
+            //notifyIcon.Icon = SystemIcons.Information;  // Use default system icon
+            //notifyIcon.Text = "My Tray App";
+            //notifyIcon.Visible = true;
+
+            //// Add context menu
+            //ContextMenuStrip menu = new ContextMenuStrip();
+            //menu.Items.Add("Show App", null, (s, e) => this.Show());
+            //menu.Items.Add("Exit", null, (s, e) => Application.Exit());
+
+            //notifyIcon.ContextMenuStrip = menu;
+
+            //// Show balloon notification
+            //notifyIcon.ShowBalloonTip(3000, "Hello!", "App is running in system tray.", ToolTipIcon.Info);
+
+            //this.Resize += (s, e) =>
+            //{
+            //    if (this.WindowState == FormWindowState.Minimized)
+            //        this.Hide()The remote name could not be resolved: 'via.placeholder.com';  // Hide window when minimized
+            //};
+            //this.Text = "PictureBox from URL";
+            //this.Size = new System.Drawing.Size(400, 400);
+
+            //PictureBox pictureBox = new PictureBox();
+            //pictureBox.Dock = DockStyle.Fill;
+            //pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+
+            //// Load image from the internet
+            //pictureBox.Load("https://imgs.search.brave.com/tu80peDDYz46QJ-k5hQt-xJBiKLPdXDaWtkdTw-6rH8/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4t/ZnJvbnQuZnJlZXBp/ay5jb20vaG9tZS9h/bm9uLXJ2bXAvY3Jl/YXRpdmUtc3VpdGUv/YXVkaW8tY3JlYXRp/b24vdmlzdWFscy1z/b25ncy53ZWJw");
+
+            //this.Controls.Add(pictureBox);
+            //this.Text = "ToolTip Example";
+            //this.Size = new System.Drawing.Size(300, 200);
+
+            //Button btn = new Button();
+            //btn.Text = "Save";
+            //btn.Location = new System.Drawing.Point(50, 50);
+
+            //TextBox txt = new TextBox();
+            //txt.Location = new System.Drawing.Point(50, 100);
+
+            //// Create ToolTip
+            //ToolTip toolTip = new ToolTip();
+            //toolTip.SetToolTip(btn, "Click here to save your work.");
+            //toolTip.SetToolTip(txt, "Enter your name here.");
+
+            //this.Controls.Add(btn);
+            //this.Controls.Add(txt);
+            //TreeView treeView = new TreeView();
+            //treeView.Dock = DockStyle.Fill;
+
+            //// Add root nodes
+            //TreeNode root1 = new TreeNode("Fruits");
+            //TreeNode root2 = new TreeNode("Vegetables");
+
+            //// Add child nodes
+            //root1.Nodes.Add("Apple");
+            //root1.Nodes.Add("Banana");
+            //root1.Nodes.Add("Mango");
+
+            //root2.Nodes.Add("Carrot");
+            //root2.Nodes.Add("Tomato");
+            //root2.Nodes.Add("Potato");
+
+            //// Add root nodes to TreeView
+            //treeView.Nodes.Add(root1);
+            //treeView.Nodes.Add(root2);
+
+            //// Handle node selection
+            //treeView.AfterSelect += (s, e) =>
+            //{
+            //    MessageBox.Show("You selected: " + e.Node.Text);
+            //};
+
+            //this.Controls.Add(treeView);
+
+            //WebBrowser webBrowser = new WebBrowser();
+            //webBrowser.Dock = DockStyle.Fill;
+
+            //// Navigate to a website
+
+            //webBrowser.Navigate("youtube.com");
+
+            //// Handle page load completed
+            //webBrowser.DocumentCompleted += (s, e) =>
+            //{
+            //    MessageBox.Show("Page Loaded: " + webBrowser.Url);
+            //};
+
+            //this.Controls.Add(webBrowser);
+            NotifyIcon nf = new NotifyIcon();
+            nf.Icon = SystemIcons.Warning;
+            nf.Visible = true;
+            nf.BalloonTipTitle = "hi";
+            nf.BalloonTipText = "This is a simple notification.";
+            nf.ShowBalloonTip(100000000);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            //MessageBox.Show($"Hello {textBox1.Text} {textBox2.Text}");
-            //textBox3.Text = $"Hello {textBox1.Text} {textBox2.Text}";
-            //progressBar1.Value += 10;
-            Form2 frm = new Form2();
-            frm.Show();
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
+            linkLabel1.Text = "hi";
         }
     }
-} 
+}
