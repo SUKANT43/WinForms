d65cd8943c5af9f648203cbb1151c8808ca7ede6
SUKANT43 <sukant98657@gmail.com>
2025-11-04 12:06:39 +0530

    drag panel bug removed

--- exc3/exc3/Form1.cs
+++ exc3/exc3/Form1.cs
@@ -1,25 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace exc3
 {
     public partial class Form1 : Form
     {
-        private bool isDragging=false;
         private Point dragStartPoint;
 
         public Form1()
         {
             InitializeComponent();
             MouseClick += ButtonClick;
-            dragPanel.MouseClick += ButtonClick;
+            dragPanel.MouseClick += DragButtonClick;
 
             dragPanel.MouseDown += MouseDownDrag;
             dragPanel.MouseUp += MouseUpDrag;
@@ -45,17 +38,36 @@ namespace exc3
             }
         }
 
-        public void MouseDownDrag(object s,MouseEventArgs e)
+        private void DragButtonClick(object s, MouseEventArgs e)
+        {
+            Point np = dragPanel.Location;
+            if (e.Button == MouseButtons.Left)
+            {
+                int x = (np.X+e.X) - (dragPanel.Width / 2);
+                int y = (np.Y+e.Y) - (dragPanel.Height / 2);
+                x = Math.Max(0, Math.Min(x, ClientSize.Width - dragPanel.Width));
+                y = Math.Max(0, Math.Min(y, ClientSize.Height - dragPanel.Height));
+
+                dragPanel.Location = new Point(x, y);
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                dragPanel.Size = new Size(100, 100);
+                dragPanel.Location = new Point(0, 0);
+            }
+        }
+
+        private void MouseDownDrag(object s,MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Middle)
             {
-                isDragging = true;
                 dragStartPoint = e.Location;
                 dragPanel.Cursor = Cursors.SizeAll;
             }
         }
 
-        public void MouseMoveDrag(object s, MouseEventArgs e)
+        private void MouseMoveDrag(object s, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Middle)
             {
@@ -72,13 +84,12 @@ namespace exc3
             }
         }
 
-                private void MouseUpDrag(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Middle)
-            {
-                isDragging = false;
-                dragPanel.Cursor = Cursors.Default;
-            }
+        private void MouseUpDrag(object sender, MouseEventArgs e)
+          {
+              if (e.Button == MouseButtons.Middle)
+              {
+                  dragPanel.Cursor = Cursors.Default;
+              }
         }
 
     }
