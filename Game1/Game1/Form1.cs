d77f75a71fd7667351ae2622ed2b075a3ccc1b74
SUKANT43 <sukant98657@gmail.com>
2025-11-05 17:34:27 +0530

    game1

--- Game1/Game1/Form1.cs
+++ Game1/Game1/Form1.cs
@@ -12,9 +12,34 @@ namespace Game1
 {
     public partial class Form1 : Form
     {
+        int x, y, h, w;
+        Panel mainPanel = new Panel();
         public Form1()
         {
             InitializeComponent();
+            //MinimumSize = new Size(700, 500);
+            //MaximumSize = new Size(700, 500);
+
+            h = Height;
+            w = Width;
+
+            mainPanel.Dock = DockStyle.Fill;
+            mainPanel.Paint += PanelDrawing;
+            //Controls.Add(mainPanel);
+            mainPanel.SendToBack();
+            //mainPanel.SendToBack();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            w = w / 2;
+            mainPanel.Invalidate();
+        }
+
+        private void PanelDrawing(object s, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawLine(Pens.Black,0,0,w,h);
         }
     }
 }
