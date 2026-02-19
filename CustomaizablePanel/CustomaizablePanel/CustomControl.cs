a317e3520fa60b43b31dea5ead8a1a95a15caded
SUKANT43 <sukant98657@gmail.com>
2026-02-19 14:43:31 +0530

    drag panel

--- CustomaizablePanel/CustomaizablePanel/CustomControl.cs
+++ CustomaizablePanel/CustomaizablePanel/CustomControl.cs
@@ -5,57 +5,50 @@ using System.Windows.Forms;
 
 namespace CustomaizablePanel
 {
+
     class CustomControl : Control
     {
-        public int Row { get; set; } = 10;
-        public int Column { get; set; } = 10;
-
+        public int Row { get; set; } = 6;
+        public int Col { get; set; } = 6;
         List<int> rowHeights = new List<int>();
         List<int> colWidths = new List<int>();
-
         const int MIN = 25;
-
-        enum ResizeType { None, Row, Column }
-        ResizeType resizeMode = ResizeType.None;
-
-        int resizeIndex = -1;
         bool dragging = false;
+        int resizeIndex = -1;
+        enum ResizeType
+        {
+            None,
+            Row,
+            Column
+        }
+
+        ResizeType resizeMode = ResizeType.None;
 
         public CustomControl()
         {
             DoubleBuffered = true;
-            BackColor = Color.White;
-
-            Resize += (s, e) => InitGrid();
-            MouseDown += OnMouseDownGrid;
-            MouseUp += OnMouseUpGrid;
-            MouseMove += OnMouseMoveGrid;
             Paint += PaintPage;
-
             InitGrid();
         }
 
-        // create equal cells
         public void InitGrid()
         {
-            if (Row <= 0 || Column <= 0) return;
-
+            if (Row <= 0 || Col <= 0) return;
             rowHeights.Clear();
             colWidths.Clear();
-
             int h = Height / Row;
-            int w = Width / Column;
-
+            int w = Width / Col;
             for (int i = 0; i < Row; i++)
+            {
                 rowHeights.Add(h);
-
-            for (int i = 0; i < Column; i++)
+            }
+            for (int i = 0; i < Col; i++)
+            {
                 colWidths.Add(w);
-
+            }
             Invalidate();
         }
 
-        // DRAW GRID
         public void PaintPage(object s, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
@@ -67,60 +60,60 @@ namespace CustomaizablePanel
             for (int r = 0; r < Row; r++)
             {
                 int x = 0;
-
-                for (int c = 0; c < Column; c++)
+                for (int c = 0; c < Col; c++)
                 {
                     Rectangle rect = new Rectangle(x, y, colWidths[c], rowHeights[r]);
-
                     g.DrawRectangle(p, rect);
-
-                    string txt = $"R{r + 1},C{c + 1}";
+                    string txt = $"Height:{rect.Height} Width:{rect.Width}";
                     SizeF sz = g.MeasureString(txt, f);
-
                     g.DrawString(txt, f, Brushes.Black,
                         rect.X + (rect.Width - sz.Width) / 2,
                         rect.Y + (rect.Height - sz.Height) / 2);
-
                     x += colWidths[c];
                 }
-
                 y += rowHeights[r];
             }
         }
 
-        // -------- MOUSE LOGIC --------
-
-        void OnMouseDownGrid(object s, MouseEventArgs e)
+        protected override void OnResize(EventArgs e)
         {
+            base.OnResize(e);
+            InitGrid();
+        }
+
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
             dragging = true;
         }
 
-        void OnMouseUpGrid(object s, MouseEventArgs e)
+        protected override void OnMouseUp(MouseEventArgs e)
         {
+            base.OnMouseUp(e);
             dragging = false;
             resizeMode = ResizeType.None;
             Cursor = Cursors.Default;
         }
 
-        void OnMouseMoveGrid(object s, MouseEventArgs e)
+        protected override void OnMouseMove(MouseEventArgs e)
         {
+            base.OnMouseMove(e);
             if (!dragging)
             {
                 DetectResizeLine(e);
                 return;
             }
-
             ResizeGrid(e);
+
         }
 
-        // detect near line
-        void DetectResizeLine(MouseEventArgs e)
+        private void DetectResizeLine(MouseEventArgs e)
         {
             resizeMode = ResizeType.None;
             Cursor = Cursors.Default;
-
             int y = 0;
-            for (int i = 0; i < Row - 1; i++)
+            for (int i = 0; i < Row; i++)
             {
                 y += rowHeights[i];
                 if (Math.Abs(e.Y - y) < 4)
@@ -133,7 +126,7 @@ namespace CustomaizablePanel
             }
 
             int x = 0;
-            for (int i = 0; i < Column - 1; i++)
+            for (int i = 0; i < Col; i++)
             {
                 x += colWidths[i];
                 if (Math.Abs(e.X - x) < 4)
@@ -146,14 +139,12 @@ namespace CustomaizablePanel
             }
         }
 
-        // resize logic
-        void ResizeGrid(MouseEventArgs e)
+        private void ResizeGrid(MouseEventArgs e)
         {
             if (resizeMode == ResizeType.Row)
             {
                 int line = GetRowLine(resizeIndex);
                 int delta = e.Y - line;
-
                 int newSize = rowHeights[resizeIndex] + delta;
                 int nextSize = rowHeights[resizeIndex + 1] - delta;
 
@@ -162,40 +153,43 @@ namespace CustomaizablePanel
                     rowHeights[resizeIndex] = newSize;
                     rowHeights[resizeIndex + 1] = nextSize;
                 }
+
             }
 
             if (resizeMode == ResizeType.Column)
             {
                 int line = GetColLine(resizeIndex);
                 int delta = e.X - line;
-
-                int newSize = colWidths[resizeIndex] + delta;
+                int newSize = colWidths[resizeIndex]+delta;
                 int nextSize = colWidths[resizeIndex + 1] - delta;
-
                 if (newSize > MIN && nextSize > MIN)
                 {
                     colWidths[resizeIndex] = newSize;
                     colWidths[resizeIndex + 1] = nextSize;
                 }
             }
-
             Invalidate();
         }
 
-        int GetRowLine(int i)
+        private int GetRowLine(int i)
         {
             int y = 0;
-            for (int k = 0; k <= i; k++)
+            for(int k = 0; k <= i; k++)
+            {
                 y += rowHeights[k];
+            }
             return y;
         }
 
-        int GetColLine(int i)
+        private int GetColLine(int i)
         {
             int x = 0;
-            for (int k = 0; k <= i; k++)
+            for(int k = 0; k <= i; k++)
+            {
                 x += colWidths[k];
+            }
             return x;
         }
+
     }
 }
