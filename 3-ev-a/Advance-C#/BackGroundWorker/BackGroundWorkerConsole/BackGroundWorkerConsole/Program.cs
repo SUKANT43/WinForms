0ba9fb18e0a76aa75a3f0f4586970afb782f111d
SUKANT43 <sukant98657@gmail.com>
2026-04-14 13:12:35 +0530

    BackGroundWorker

---
+++ 3-ev-a/Advance-C#/BackGroundWorker/BackGroundWorkerConsole/BackGroundWorkerConsole/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Threading;
+
+namespace BackGroundWorkerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += (s, e) =>
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    worker.ReportProgress(i);
+                }
+            };
+
+            worker.ProgressChanged += (s, e) =>
+            {
+                Console.WriteLine(e.ProgressPercentage);
+            };
+            worker.RunWorkerAsync();
+            worker.RunWorkerCompleted += (s, e) =>
+            {
+                Console.WriteLine("Work completed");
+            };
+
+
+            // worker.WorkerSupportsCancellation = true;
+            // worker.DoWork += (s, e) =>
+            // {
+            //     for(int i = 0; i <= 10; i++)
+            //     {
+            //         if (worker.CancellationPending)
+            //         {
+            //             e.Cancel = true;
+            //             return;
+            //         }
+            //         Thread.Sleep(50);
+            //         worker.ReportProgress(i);
+            //     }
+            // };
+            // worker.ProgressChanged += (s, e) =>
+            // {
+            //     Console.WriteLine(e.ProgressPercentage);
+            // };
+
+            // worker.RunWorkerAsync();
+            //worker.CancelAsync();
+             Console.ReadLine();
+
+        }
+    }
+}
